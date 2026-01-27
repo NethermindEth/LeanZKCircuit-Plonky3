490baa9a9be0a9b22f58cbf757152aad8122a0ed
Dom Henderson <dom@nethermind.io>
2026-01-27 12:06:33 -0800

    Set to 4.27.0

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ open System Lake DSL
 package LeanZKCircuit_Plonky3 where version := v!"0.1.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git"@"v4.27.0"
 
 @[default_target]
 lean_lib LeanZKCircuit_Plonky3
